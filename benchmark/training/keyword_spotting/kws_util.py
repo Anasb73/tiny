1970211c3ced48c4ef06e2aafe021441cd7f5d50
Anas Benalla <anas.benalla@asygn.com>
2024-09-25 18:08:50 +0200

    statrt form kws ref pretrained

--- benchmark/training/keyword_spotting/kws_util.py
+++ benchmark/training/keyword_spotting/kws_util.py
@@ -146,7 +146,8 @@ def parse_command():
       '--model_init_path',
       type=str,
       #default=None,
-      default="/work1/gitlab-runner-docker-data/models/keyword_spotting/trained_models/kws_model.h5",
+      #default="/work1/gitlab-runner-docker-data/models/keyword_spotting/trained_models/kws_model.h5",
+      default="/work1/gitlab-runner-docker-data/models/keyword_spotting/trained_models/kws_ref_model",
       help='Path to load pretrained model for evaluation or starting point for training')
   parser.add_argument(
       '--tfl_file_name',
