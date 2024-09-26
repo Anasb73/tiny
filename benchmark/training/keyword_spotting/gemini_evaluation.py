f5aa8d3cfe78217586dae6101002a93330d7039c
Anas Benalla <anas.benalla@asygn.com>
2024-09-26 18:23:32 +0200

    update gemini evaluation

--- benchmark/training/keyword_spotting/gemini_evaluation.py
+++ benchmark/training/keyword_spotting/gemini_evaluation.py
@@ -16,12 +16,14 @@ def run_gemini(gemini_path, model_path, sample_path, output_file="/work1/gitlab-
     # Construct the command with the full path to the gemini executable
     cmd = [gemini_path, model_path, sample_path]
     
-    log_file = "/work1/gitlab-runner-docker-data/ATHENAP18/FROM_ST_TO_ASYGN/tensorflow_gemini/geminipyc128PE/log.txt"
     with open(log_file, 'w') as log:
         # Run the command, writing stdout and stderr to the log file
-        subprocess.run(cmd, stdout=log, stderr=subprocess.STDOUT)
+        subprocess.run(cmd)
 
-    # Read predictions from the output file (output_gemini.txt)
+
+    directory = os.path.dirname(output_file)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
     with open(output_file, 'r') as f:
         predictions = [int(line.strip()) for line in f.readlines()]
         print("predictions:", predictions)
